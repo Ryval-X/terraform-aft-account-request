36459111e6fb56d999bcd38ec52be7098072ee92
kartish <kreddy@ryvalx.com>
2025-07-17 21:08:10 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "App"
 }
+
+module "Test_Acc" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "twx-kartish4@ryvalx.com"
+    AccountName               = "Test"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "kartish@ryvalx.com"
+    SSOUserFirstName          = "kartish"
+    SSOUserLastName           = "R"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Security"
+}
