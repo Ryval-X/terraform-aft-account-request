a319eef833fbf0e71c3fa4007863db40d6be49a1
kartish <kreddy@ryvalx.com>
2025-07-18 09:27:34 +0530

    new account request

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "sandbox" {
 
   control_tower_parameters = {
     AccountEmail              = "twx-kartish5@ryvalx.com"
-    AccountName               = "Dev"
+    AccountName               = "ash"
     ManagedOrganizationalUnit = "App"
     SSOUserEmail              = "kartish@ryvalx.com"
     SSOUserFirstName          = "kartish"
@@ -26,30 +26,30 @@ module "sandbox" {
   account_customizations_name = "App"
 }
 
-module "Test_Acc" {
-  source = "./modules/aft-account-request"
+# module "Test_Acc" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "twx-kartish4@ryvalx.com"
-    AccountName               = "Test"
-    ManagedOrganizationalUnit = "Security"
-    SSOUserEmail              = "kartish@ryvalx.com"
-    SSOUserFirstName          = "kartish"
-    SSOUserLastName           = "R"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "twx-kartish4@ryvalx.com"
+#     AccountName               = "Test"
+#     ManagedOrganizationalUnit = "Security"
+#     SSOUserEmail              = "kartish@ryvalx.com"
+#     SSOUserFirstName          = "kartish"
+#     SSOUserLastName           = "R"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "Security"
-}
+#   account_customizations_name = "Security"
+# }
